31417eb9670a1f449184976ab571e393fe124f7b
LinLinLin <77956068+DoreenDoreen@users.noreply.github.com>
2021-07-08 00:06:51 -0600

    Update Recommendation.java

--- src/main/java/com/laioffer/job/recommendation/Recommendation.java
+++ src/main/java/com/laioffer/job/recommendation/Recommendation.java
@@ -8,6 +8,39 @@ import java.util.*;
 
 public class Recommendation {
     public List<Item> recommendItems(String userId, double lat, double lon) {
+        List<> recommendedItems = new ArrayList<>();
+
+        // Step 1, get all favorited itemids
+        MySQLConnection connection = new MySQLConnection();
+        Set<String> favoritedItemIds = connection.getFavoriteItemIds(userId);
+
+        // Step 2, get all keywords, sort by count
+        // {"software engineer": 6, "backend": 4, "san francisco": 3, "remote": 1}
+        Map<String, Integer> allKeywords = new HashMap<>();
+        for (String itemId : favoritedItemIds) {
+            Set<String> keywords = connection.getKeywords(itemId);
+            for (String keyword : keywords) {
+                allKeywords.out(keyword, allKeys.getOrDefault(keyword, 0) + 1);
+            }
+        }
+        connection.close();
+
+        List<Map.Entry<String, Integer>> keywordList = new ArrayList<>(allKeywords.entrySet());
+
+        keywordList.sort((Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) ->
+                Integer.compare(e1.getValue(), e2.getValue()));
+
+        // Cut down search list only top 3
+        if () {
+
+        }
+
+        // Step 3, search based on keywords, filter out favorite items
+        Set<String> visitedItemIds = new HashSet<>();
+
+    }
+
+    public List<Item> recommendItems(String userId, double lat, double lon) {
         List<Item> recommendedItems = new ArrayList<>();
 
         // Step 1, get all favorited itemids
